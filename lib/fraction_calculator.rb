309611126b8852e1cd6580f9d60d11a7a4ef2000
Tom <thomas.spencer@codurance.com>
2020-06-13 12:24:23 +0100

    Refactor

--- lib/fraction_calculator.rb
+++ lib/fraction_calculator.rb
@@ -4,13 +4,5 @@ def add(input)
     (fractions[0].split('/')[1].to_i * fractions[1].split('/')[0].to_i)
   denominator = fractions[0].split('/')[1].to_i * fractions[1].split('/')[1].to_i
 
-  if fractions[0] == '7/3'
-    return "#{numerator}/#{fractions[0].split('/')[1].to_i * fractions[1].split('/')[1].to_i}"
-  end
-
-  if fractions[0] == '1/8'
-    return "#{numerator}/32"
-  end
-
   "#{numerator}/#{denominator}"
 end
