7a6253264a5534e06f95d17711eeaa1d5baf85ba
Tom <thomas.spencer@codurance.com>
2020-06-13 14:01:59 +0100

    Add each for denominator

--- lib/fraction_calculator.rb
+++ lib/fraction_calculator.rb
@@ -1,18 +1,22 @@
-# frozen_string_literal: true
 
 def add(input)
-  fractions = input.split(',').map { |fr| fr.split('/').map(&:to_i) }
+  fractions = input.split(',').map{|fr| fr.split('/').map{|i| i.to_i } }
   numerator = numerate(fractions)
   denominator = denominate(fractions)
   gcd = numerator.gcd(denominator)
 
-  "#{numerator / gcd}/#{denominator / gcd}"
+  "#{numerator / gcd }/#{denominator / gcd }"
 end
+
 
 def numerate(fractions)
   (fractions[0][0] * fractions[1][1]) + (fractions[0][1] * fractions[1][0])
 end
 
 def denominate(fractions)
-  fractions[0][1] * fractions[1][1]
+  result = 1
+  fractions.each do |i|
+    result *= i[1]
+  end
+  result
 end
