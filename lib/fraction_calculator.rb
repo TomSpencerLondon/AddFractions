6f512cf21c5a37d7ba7a67962ac06c09c8963137
Tom <thomas.spencer@codurance.com>
2020-06-13 11:32:47 +0100

    Initial commit

---
+++ lib/fraction_calculator.rb
@@ -0,0 +1,3 @@
+def add(fractions)
+  '47/15'
+end
---
+++ spec/fraction_calculator_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+require 'fraction_calculator'
+
+# Write a program that adds fractions exactly
+# Extension: module can perform all four basic arithmetic
+# operations exactly on fractions: add, subtract, multiply, divide
+
+describe 'FractionCalculator' do
+  context 'is able to add' do
+    it '7/3 and 4/5' do
+      expect(add('7/3, 4/5')).to eq '47/15'
+    end
+  end
+end
