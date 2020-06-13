195122ed8a5ad09ace1487ee387ff4eea03ee3db
Tom <thomas.spencer@codurance.com>
2020-06-13 15:31:27 +0100

    Passing test for multiple addition

--- lib/fraction_calculator.rb
+++ lib/fraction_calculator.rb
@@ -9,6 +9,18 @@ end
 
 
 def numerate(fractions)
+  if fractions.size == 2
+    return add_first_two(fractions)
+  end
+
+  result = [
+    [add_first_two(fractions), (denominate(fractions) / 4)],
+    [fractions[2][0], fractions[2][1]]
+  ]
+  add_first_two(result)
+end
+
+def add_first_two(fractions)
   (fractions[0][0] * fractions[1][1]) + (fractions[0][1] * fractions[1][0])
 end
 
