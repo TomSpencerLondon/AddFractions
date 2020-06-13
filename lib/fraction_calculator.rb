9e14dbe494d77063892b867fb4cf49dd2e040eb2
Tom <thomas.spencer@codurance.com>
2020-06-13 12:36:48 +0100

    refactor methods

--- lib/fraction_calculator.rb
+++ lib/fraction_calculator.rb
@@ -1,8 +1,18 @@
+
 def add(input)
   fractions = input.split(',')
-  numerator = (fractions[0].split('/')[0].to_i * fractions[1].split('/')[1].to_i) +
-    (fractions[0].split('/')[1].to_i * fractions[1].split('/')[0].to_i)
-  denominator = fractions[0].split('/')[1].to_i * fractions[1].split('/')[1].to_i
+  numerator = numerate(fractions)
+  denominator = denominate(fractions)
 
   "#{numerator}/#{denominator}"
+end
+
+
+def numerate(fractions)
+  (fractions[0].split('/')[0].to_i * fractions[1].split('/')[1].to_i) +
+  (fractions[0].split('/')[1].to_i * fractions[1].split('/')[0].to_i)
+end
+
+def denominate(fractions)
+  fractions[0].split('/')[1].to_i * fractions[1].split('/')[1].to_i
 end
